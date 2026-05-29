c8c5c26cf1ec8fdcfad518c0b2af484ff460cd39
Sameer Sundresh <sameer@sundresh.org>
2026-05-28 18:35:24 -0600

    Add unit tests for utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -100,3 +100,124 @@ impl<'a> ParseInt<u32> for std::path::Component<'a> {
         self.as_os_str().parse_int()
     }
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// tests
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::collections::HashSet;
+    use std::fs::File;
+    use tempfile::tempdir;
+
+    // Test dir_contents()
+
+    #[test]
+    fn test_dir_contents_empty() {
+        let tempdir = tempdir().expect("failed to create temporary directory with tempdir()");
+        let actual_paths = dir_contents(tempdir.path()).expect("dir_contents() did not return an iterator").collect::<HashSet<PathBuf>>();
+        assert_eq!(HashSet::new(), actual_paths);
+    }
+
+    #[test]
+    fn test_dir_contents_nonempty() {
+        let tempdir = tempdir().expect("failed to create temporary directory with tempdir()");
+        let expected_paths = HashSet::from([tempdir.path().join("abc"), tempdir.path().join("xyz"), tempdir.path().join("123")]);
+        for path in &expected_paths {
+            File::create(path).expect(&format!("failed to create file in tempdir: {}", path.to_str().unwrap()));
+        }
+        let actual_paths = dir_contents(tempdir.path()).expect("dir_contents() did not return an iterator").collect::<HashSet<PathBuf>>();
+        assert_eq!(expected_paths, actual_paths);
+    }
+
+    #[test]
+    fn test_dir_contents_nonexistent() {
+        let tempdir = tempdir().expect("failed to create temporary directory with tempdir()");
+        assert!(dir_contents(&tempdir.path().join("does_not_exist")).is_err());
+    }
+
+    // Test ensure_symlink_exists()
+
+    #[test]
+    fn test_ensure_symlink_exists_initially_nonexitent() {
+        let tempdir = tempdir().expect("failed to create temporary directory with tempdir()");
+        let symlink_path = &tempdir.path().join("link");
+        let target_path = Path::new("/");
+        ensure_symlink_exists(symlink_path, &target_path).expect("error in ensure_symlink_exists()");
+        assert_eq!(target_path, read_link(symlink_path).expect("error reading symlink"));
+    }
+
+    #[test]
+    fn test_ensure_symlink_exists_initially_correct() {
+        let tempdir = tempdir().expect("failed to create temporary directory with tempdir()");
+        let symlink_path = &tempdir.path().join("link");
+        let target_path = Path::new("/");
+        symlink(target_path, symlink_path).expect("error creating symlink");
+        ensure_symlink_exists(symlink_path, &target_path).expect("error in ensure_symlink_exists()");
+        assert_eq!(target_path, read_link(symlink_path).expect("error reading symlink"));
+    }
+
+    #[test]
+    fn test_ensure_symlink_exists_initially_incorrect() {
+        let tempdir = tempdir().expect("failed to create temporary directory with tempdir()");
+        let symlink_path = &tempdir.path().join("link");
+        let target_path = Path::new("/");
+        symlink(Path::new("invalid"), symlink_path).expect("error creating symlink");
+        ensure_symlink_exists(symlink_path, &target_path).expect("error in ensure_symlink_exists()");
+        assert_eq!(target_path, read_link(symlink_path).expect("error reading symlink"));
+    }
+
+    // Test file_name_as_number()
+
+    #[test]
+    fn test_file_name_as_number() {
+        assert_eq!(None, file_name_as_number(Path::new("/abc/jkl/x")));
+        assert_eq!(None, file_name_as_number(Path::new("/abc/jkl/x123")));
+        assert_eq!(None, file_name_as_number(Path::new("/abc/jkl/123x")));
+        assert_eq!(Some(0), file_name_as_number(Path::new("/abc/jkl/0/")));
+        assert_eq!(Some(123), file_name_as_number(Path::new("/abc/jkl/123")));
+    }
+
+    // Test file_name_is_number()
+
+    #[test]
+    fn test_file_name_is_number() {
+        assert!(!file_name_is_number(Path::new("/abc/jkl/x")));
+        assert!(file_name_is_number(Path::new("/abc/jkl/123")));
+    }
+
+    // Test sorted_numeric_entries_in_dir()
+
+    #[test]
+    fn test_sorted_numeric_entries_in_dir() {
+        let tempdir = tempdir().expect("failed to create temporary directory with tempdir()");
+        let all_file_names = ["a", "0", "b", "1", "cd", "23", "efg", "456", "jklmno"];
+        let numeric_file_names = ["0", "1", "23", "456"];
+        let all_file_paths = all_file_names.map(|filename| tempdir.path().join(filename));
+        let expected_result: Vec<PathBuf> = numeric_file_names.map(|filename| tempdir.path().join(filename)).into();
+        for path in all_file_paths {
+            File::create(&path).expect(&format!("failed to create file in tempdir: {}", path.to_str().unwrap()));
+        }
+        let actual_result = sorted_numeric_entries_in_dir(tempdir.path());
+        assert_eq!(expected_result, actual_result);
+    }
+
+    // Test is_plus_or_minus_int()
+
+    #[test]
+    fn test_is_plus_or_minus_int_false() {
+        assert!(!is_plus_or_minus_int(""));
+        assert!(!is_plus_or_minus_int("1"));
+        assert!(!is_plus_or_minus_int("hi"));
+        assert!(!is_plus_or_minus_int("+1."));
+        assert!(!is_plus_or_minus_int("-1."));
+    }
+
+    #[test]
+    fn test_is_plus_or_minus_int_true() {
+        assert!(is_plus_or_minus_int("+0"));
+        assert!(is_plus_or_minus_int("+1"));
+        assert!(is_plus_or_minus_int("-2"));
+    }
+}
